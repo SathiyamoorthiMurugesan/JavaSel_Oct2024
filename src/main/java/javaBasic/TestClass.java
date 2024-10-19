da5ed3a47d7ff41605c8e2ff7f1efe10d74da082
Haridass-M <haridassm2809@gmail.com>
2024-10-19 14:44:19 +0530

    tried examples of class, interface, method and variable

---
+++ src/main/java/javaBasic/TestClass.java
@@ -0,0 +1,14 @@
+package javaBasic;
+
+public class TestClass {
+
+private class Test2 {
+	
+}
+}
+//public class Test3{
+//} - can't able to create another class of class
+class test3{
+	
+} //but we can create default class outside the class also
+
---
+++ src/main/java/javaBasic/TestInterface.java
@@ -0,0 +1,14 @@
+package javaBasic;
+
+public interface TestInterface {
+
+//	private interface Test2{
+		
+//	} we can't create another interface inside one interface, only we can create default interface
+	interface Test2{
+		
+	}
+}
+//public interface Test3{
+	
+//}
